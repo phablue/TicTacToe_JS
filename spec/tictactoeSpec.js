d4917588ae9bec48f42ae0e4acee3a1c87d897dc
hyunahpark <phablue@gmail.com>
2014-02-28 11:13:25 -0600

    fixed code

--- spec/tictactoeSpec.js
+++ spec/tictactoeSpec.js
@@ -1,98 +1,99 @@
 describe ("Change a player to", function() {
   it ("'O', if a current player is 'X'", function() {
-    current_player = "X";
-    expect (tictactoe.change_player(current_player)).toBe("O");
+    tictactoe.current_player = "X";
+    expect (tictactoe.change_player()).toBe("O");
   });
   it ("'X', if a current player is 'O'", function() {
-    current_player = "O";
-    expect (tictactoe.change_player(current_player)).toBe("X");
+    tictactoe.current_player = "O";
+    expect (tictactoe.change_player()).toBe("X");
   });
 });
 
+
 describe ("Get vailable spots from a board", function() {
   it ("vailable spots are [3, 5, 7, 9]", function() {
-    var board = ["X", "O", 3, "X", 5, "O", 7, "X", 9];
-    expect (tictactoe.valid_spots(board)).toEqual([3, 5, 7, 9]);
+    tictactoe.board = ["X", "O", 3, "X", 5, "O", 7, "X", 9];
+    expect (tictactoe.valid_spots()).toEqual([3, 5, 7, 9]);
   });
   it ("vailable spots are [1, 3, 5, 9]", function() {
-    var board = [1, "O", 3, "X", 5, "O", "X", "X", 9];
-    expect (tictactoe.valid_spots(board)).toEqual([1, 3, 5, 9]);
+    tictactoe.board = [1, "O", 3, "X", 5, "O", "X", "X", 9];
+    expect (tictactoe.valid_spots()).toEqual([1, 3, 5, 9]);
   });
 });
 
 describe ("Win a Game", function() {
   describe ("If any line of board is 'X,X,X' or 'O,O,O'", function() {
     it ("Win, when 1, 2, 3 spots are 'X'", function() {
-      board = ["X", "X", "X", 4, 5, 6, 7, 8, 9];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board = ["X", "X", "X", 4, 5, 6, 7, 8, 9];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 1, 2, 3 spots are 'X, O, X'", function() {
-      board = ["X", "O", "X", 4, 5, 6, 7, 8, 9];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board = ["X", "O", "X", 4, 5, 6, 7, 8, 9];
+      expect (tictactoe.game_win()).toBe(false);
     });
     it ("Win, when 4, 5, 6 spots are 'O'", function() {
-      board = [1, 2, 3, "O", "O", "O", 7, 8, 9];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board = [1, 2, 3, "O", "O", "O", 7, 8, 9];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 4, 5, 6 spots are 'X, X, O'", function() {
-      board = [1, 2, 3, "X", "X", "O", 7, 8, 9];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board = [1, 2, 3, "X", "X", "O", 7, 8, 9];
+      expect (tictactoe.game_win()).toBe(false);
     });
     it ("Win, when 7, 8, 9 spots are 'O'", function() {
-      board = [1, 2, 3, 4, 5, 6, "O", "O", "O"];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board = [1, 2, 3, 4, 5, 6, "O", "O", "O"];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 7, 8, 9 spots are 'O, X, O'", function() {
-      board = [1, 2, 3, 4, 5, 6, "O", "X", "O"];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board = [1, 2, 3, 4, 5, 6, "O", "X", "O"];
+      expect (tictactoe.game_win()).toBe(false);
     });
   });
   describe ("If any cloumn of board is 'X,X,X' or 'O,O,O'", function() {
     it ("Win, when 1, 4, 7 spots are 'X'", function() {
-      board = ["X", 2, 3, "X", 5, 6, "X", 8, 9];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board  = ["X", 2, 3, "X", 5, 6, "X", 8, 9];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 1, 4, 7 spots are 'X, O, X'", function() {
-      board = ["X", 2, 3, "O", 5, 6, "X", 8, 9];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board  = ["X", 2, 3, "O", 5, 6, "X", 8, 9];
+      expect (tictactoe.game_win()).toBe(false);
     });
     it ("Win, when 2, 5, 8 spots are 'O'", function() {
-      board = [1, "O", 3, 4, "O", 6, 7, "O", 9];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board  = [1, "O", 3, 4, "O", 6, 7, "O", 9];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 2, 5, 8 spots are 'X, X, O'", function() {
-      board = [1, "X", 3, 4, "X", 6, 7, "O", 9];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board  = [1, "X", 3, 4, "X", 6, 7, "O", 9];
+      expect (tictactoe.game_win()).toBe(false);
     });
     it ("Win, when 3, 6, 9 spots are 'O'", function() {
-      board = [1, 2, "O", 4, 5, "O", 7, 8, "O"];
-      expect (tictactoe.game_win(board)).toBe(true);
+      tictactoe.board  = [1, 2, "O", 4, 5, "O", 7, 8, "O"];
+      expect (tictactoe.game_win()).toBe(true);
     });
     it ("Lose, when 3, 6, 9 spots are 'O, X, O'", function() {
-      board = [1, 2, "O", 4, 5, "X", 7, 8, "O"];
-      expect (tictactoe.game_win(board)).toBe(false);
+      tictactoe.board  = [1, 2, "O", 4, 5, "X", 7, 8, "O"];
+      expect (tictactoe.game_win()).toBe(false);
     });
   });
 });
 
 describe ("Game is tied", function() {
   it ("If board doesn't have any number", function() {
-    board = ["X", "X", "X", 4, 5, 6, 7, 8, 9];
-    expect (tictactoe.game_tie(board)).toBe(false);
+    tictactoe.board = ["X", "X", "X", 4, 5, 6, 7, 8, 9];
+    expect (tictactoe.game_tie()).toBe(false);
   });
   it ("If board doesn't have any number", function() {
-    board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
-    expect (tictactoe.game_tie(board)).toBe(true);
+    tictactoe.board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+    expect (tictactoe.game_tie()).toBe(true);
   });
 });
 
 describe ("Game is over", function() {
   it ("If game is tie", function() {
-    board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
-    expect (tictactoe.game_over(board)).toBe(true);
+    tictactoe.board = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+    expect (tictactoe.game_over()).toBe(true);
   });
   it ("If win a game", function() {
-    board = [1, 2, "O", 4, 5, "O", 7, 8, "O"];
-    expect (tictactoe.game_over(board)).toBe(true);
+    tictactoe.board = [1, 2, "O", 4, 5, "O", 7, 8, "O"];
+    expect (tictactoe.game_over()).toBe(true);
   });
 });
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -2,43 +2,51 @@ var tictactoe = {
   board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
   current_player: "X",
 
-  change_player: function(current_player) {
-    return current_player = (current_player == "X" ? "O" : "X");
+  valid_spots: function() {
+    var available_spots = [];
+    for (spot in this.board) {
+      if (typeof(this.board[spot]) === "number") {
+        available_spots.push(this.board[spot]);
+      }
+    }
+    return available_spots;
   },
 
-  mark_chosen_spot: function(e) {
-    var chosen_spot;
+  change_player: function() {
+    return this.current_player = (this.current_player == "X" ? "O" : "X");
+  },
+
+  choice_spot: function(e) {
     if (e == null) {
       chosen_spot = event.srcElement.id;
     }
     else {
       chosen_spot = e.target.id;
     }
+    this.mark_chosen_spot(chosen_spot);
+  },
+
+  mark_chosen_spot: function(chosen_spot) {
     document.getElementById(chosen_spot).innerHTML = this.current_player;
-    console.log(this.current_player);
-    board[chosen_spot] = current_player;
-    this.change_player(current_player);
+    this.board[chosen_spot] = this.current_player;
   },
 
-  valid_spots: function(board) {
-    var available_spots = [];
-    for (spot in board) {
-      if (typeof(board[spot]) === "number") {
-        available_spots.push(board[spot]);
-      }
-    }
-    return available_spots;
+  // next_turn: function() {
+  //   this.change_player();
+  //   if(game_over()) {
+  //     alert("You win!!");
+  //   }
+  // },
+
+  game_over: function() {
+    return this.game_win(this.board) || this.game_tie(this.board);
   },
 
-  game_over: function(board) {
-    return this.game_win(board) || this.game_tie(board);
-  },
-
-  game_win: function(board) {
+  game_win: function() {
     var rules_of_win = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     for (r in rules_of_win) {
       var rule = rules_of_win[r];
-      var state = board[rule[0]] + board[rule[1]] +  board[rule[2]];
+      var state = this.board[rule[0]] + this.board[rule[1]] +  this.board[rule[2]];
       if (state == "XXX" || state == "OOO") {
         return true;
       }
@@ -46,13 +54,12 @@ var tictactoe = {
     return false;
   },
 
-  game_tie: function(board) {
-    return tie = ((this.valid_spots(board).length) ? false : true);
+  game_tie: function() {
+    return (this.valid_spots().length) ? false : true;
   },
 
   game: function() {
-      document.onclick = this.mark_chosen_spot;
-      document.onclick = this.mark_chosen_spot;
-    }
+    document.click = this.choice_spot;
+  }
 };
 tictactoe.game();
