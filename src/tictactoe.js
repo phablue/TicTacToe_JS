6c2ec5e904b5e4d884510a107f12b17cd7cd16ab
hyunahpark <phablue@gmail.com>
2014-02-27 22:31:58 -0600

    fixed code

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,63 +1,58 @@
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-current_player = "X";
+var tictactoe = {
+  board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  current_player: "X",
 
-function change_player(current_player) {
-  return current_player = (current_player == "X" ? "O" : "X");
-}
+  change_player: function(current_player) {
+    return current_player = (current_player == "X" ? "O" : "X");
+  },
 
-function mark_chosen_spot(e) {
-  var chosen_spot
-  if (e == null) {
-    chosen_spot = event.srcElement.id;
-  }
-  else {
-    chosen_spot = e.target.id;
-  }
-  document.getElementById(chosen_spot).innerHTML = current_player;
-  board[chosen_spot] = current_player;
-  change_player(current_player);
-}
-
-function valid_spots(board) {
-  var available_spots = [];
-  for (spot in board) {
-    if (typeof(board[spot]) === "number") {
-      available_spots.push(board[spot]);
+  mark_chosen_spot: function(e) {
+    var chosen_spot;
+    if (e == null) {
+      chosen_spot = event.srcElement.id;
     }
-  }
-  return available_spots;
-}
-
-function game_over(board) {
-  return game_win(board) || game_tie(board);
-}
-
-function game_win(board) {
-  var rules_of_win = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-  for (r in rules_of_win) {
-    var rule = rules_of_win[r];
-    var state = this.board[rule[0]] + this.board[rule[1]] + this.board[rule[2]];
-    if (state == "XXX" || state == "OOO") {
-      return true;
+    else {
+      chosen_spot = e.target.id;
     }
-  }
-  return false;
-}
+    document.getElementById(chosen_spot).innerHTML = this.current_player;
+    console.log(this.current_player);
+    board[chosen_spot] = current_player;
+    this.change_player(current_player);
+  },
 
-function game_tie(board) {
-  return tie = ((valid_spots(board).length) ? false : true);
-}
+  valid_spots: function(board) {
+    var available_spots = [];
+    for (spot in board) {
+      if (typeof(board[spot]) === "number") {
+        available_spots.push(board[spot]);
+      }
+    }
+    return available_spots;
+  },
 
-function game(board) {
-  game_continue = true;
-  while (game_continue) {
-    document.onclick = mark_chosen_spot;
-    if (game_over(board)) {
-      break;
+  game_over: function(board) {
+    return this.game_win(board) || this.game_tie(board);
+  },
+
+  game_win: function(board) {
+    var rules_of_win = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    for (r in rules_of_win) {
+      var rule = rules_of_win[r];
+      var state = board[rule[0]] + board[rule[1]] +  board[rule[2]];
+      if (state == "XXX" || state == "OOO") {
+        return true;
+      }
     }
-    document.onclick = mark_chosen_spot;
-    if (game_over(board)) {
-      break;
+    return false;
+  },
+
+  game_tie: function(board) {
+    return tie = ((this.valid_spots(board).length) ? false : true);
+  },
+
+  game: function() {
+      document.onclick = this.mark_chosen_spot;
+      document.onclick = this.mark_chosen_spot;
     }
-  }
-}
+};
+tictactoe.game();
