7c159daccfb2fafc01722dc1d9456671b419a719
hyunahpark <phablue@gmail.com>
2014-03-06 18:18:10 -0600

    Test resetBoard() in GameBoard

--- spec/GameBoardSpec.js
+++ spec/GameBoardSpec.js
@@ -16,4 +16,12 @@ describe ("Test a game board", function() {
       expect (gameboard.validSpots()).toEqual([1, 3, 5, 9]);
     });
   });
+
+  describe ("Reset Board", function() {
+    it ("Changes to [1, 2, 3, 4, 5, 6, 7, 8, 9]", function() {
+      gameboard.spots = ["X", "O", 3, "X", 5, "O", 7, "X", 9];
+      gameboard.resetBoard();
+      expect (gameboard.spots).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+  });
 });
