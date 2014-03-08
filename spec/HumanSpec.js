d0155ae8f692aa71561158273feb5eba96d06daf
hyunahpark <phablue@gmail.com>
2014-03-07 23:33:51 -0600

    Fixed humanSpec

--- spec/HumanSpec.js
+++ spec/HumanSpec.js
@@ -1,27 +1,21 @@
 describe ("Test Human", function() {
-  var gameboard;
-  var ui;
-  var human;
   var currentPlayer;
 
   beforeEach(function () {
-    gameboard = window.GameBoard;
-    ui = window.UI;
-    human = window.Human;
-    gameboard.spots = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    GameBoard.spots = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   });
 
   describe ("Change a current player", function() {
     it ("To 'O', if a current player is 'X'", function() {
       currentPlayer = "X"
-      human.changePlayer(currentPlayer);
-      expect (human.currentPlayer).toBe("O");
+      Human.changePlayer(currentPlayer);
+      expect (Human.currentPlayer).toBe("O");
     });
 
     it ("To 'X', if a current player is 'O'", function() {
       currentPlayer = "O";
-      human.changePlayer(currentPlayer);
-      expect (human.currentPlayer).toBe("X");
+      Human.changePlayer(currentPlayer);
+      expect (Human.currentPlayer).toBe("X");
     });
   });
 
@@ -29,33 +23,30 @@ describe ("Test Human", function() {
     it ("td #0 and board[0] value changes to 'X' and current player changes to 'O'", function() {
       currentPlayer = "X";
       setFixtures("<tr> <td id = '0'></td></tr>");
-      human = window.Human;
       expect ($("tr td")).toBeEmpty();
-      human.choiceSpot(gameboard, currentPlayer);
+      Human.choiceSpot(GameBoard, currentPlayer);
       $("tr td").click();
       expect ($("#0")).toHaveText("X");
-      expect (gameboard.spots[0]).toBe("X");
-      expect (human.currentPlayer).toBe("O");
+      expect (GameBoard.spots[0]).toBe("X");
+      expect (Human.currentPlayer).toBe("O");
     });
 
     it ("td #3 and board[3] value changes to 'O' and current player changes to 'X'", function() {
       currentPlayer = "O";
       setFixtures("<tr> <td id = '3'></td></tr>");
-      human = window.Human;
       expect ($("tr td")).toBeEmpty();
-      human.choiceSpot(gameboard, currentPlayer);
+      Human.choiceSpot(GameBoard, currentPlayer);
       $("tr td").click();
       expect ($("#3")).toHaveText("O");
-      expect (gameboard.spots[3]).toBe("O");
-      expect (human.currentPlayer).toBe("X");
+      expect (GameBoard.spots[3]).toBe("O");
+      expect (Human.currentPlayer).toBe("X");
     });
 
     it ("Can't set of a choosen spot", function() {
-      var errorMessage = ui.spotErrorMessage();
+      var errorMessage = spyOn(UI, "spotErrorMessage");
       currentPlayer = "O";
       setFixtures("<tr> <td id = '0'>X</td><td id = '3'></td></tr>");
-      human = window.Human;
-      human.choiceSpot(gameboard, currentPlayer);
+      Human.choiceSpot(GameBoard, currentPlayer);
       $("#0").click();
       expect (errorMessage).toHaveBeenCalled();
     });
