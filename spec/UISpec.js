6baa5cef7940947501060c707c88c484b462c597
hyunahpark <phablue@gmail.com>
2014-03-08 19:28:23 -0600

    Test UI.js

---
+++ spec/UISpec.js
@@ -0,0 +1,42 @@
+describe ("Test UI", function () {
+  describe ("Test alert events", function() {
+    var winner = "Player1";
+    var alert;
+
+    beforeEach (function() {
+      alert = spyOn(window, "alert");
+    });
+
+    it ("Pops up a spot Error Message", function() {
+      UI.spotErrorMessage();
+      expect(alert).toHaveBeenCalledWith("That is not an available spot.\nPlease choose a different spot.");
+    });
+
+    it ("Pops up a win Message", function() {
+      UI.winMessage(winner);
+      expect(alert).toHaveBeenCalledWith("Congratulations.\n"+ winner +" win!!");
+    });
+
+    it ("Pops up a tie Message", function() {
+      UI.tieMessage();
+      expect(alert).toHaveBeenCalledWith("Game is tied.\nGame Over.");
+    });
+
+    it ("Pops up a inputError Message", function() {
+      UI.inputErrorMessage();
+      expect(alert).toHaveBeenCalledWith("You have to choose 'X' or 'O'.");
+    });
+  });
+
+  describe ("Test prompt events", function() {
+    var prompt;
+    var chooesnPlayer;
+    it ("Pops up a message to ask about choosen player", function() {
+      prompt = spyOn(window, "prompt").and.returnValue("X");
+      chooesnPlayer = UI.askChoicePlayer();
+      UI.askChoicePlayer();
+      expect(prompt).toHaveBeenCalledWith("What player do you require? ('X' or 'O'):");
+      expect(chooesnPlayer).toEqual("X");
+    });
+  });
+});
