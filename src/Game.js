361e63b3ddc05934e4d7fc0c890f84f4c74c904e
hyunahpark <phablue@gmail.com>
2014-03-10 16:36:41 -0500

    fixed newGame() and play() and restartGame() in Game

--- src/Game.js
+++ src/Game.js
@@ -15,11 +15,21 @@
       }
       else if(input == "X") {
         this.currentPlayer = "X";
-        ui.toggleDisplayedButton(".btn-start", ".btn-new");
+        if ($(event.target).attr('class') == "btn btn-start") {
+          ui.toggleDisplayedButton(".btn-start", ".btn-new");
+        }
+        else {
+          ui.toggleDisplayedButton(".btn-restart", ".btn-new");
+        }
       }
       else if(input == "O") {
         this.currentPlayer = "O";
-        ui.toggleDisplayedButton(".btn-start", ".btn-new");
+        if($(event.target).attr('class') == "btn .btn-start") {
+          ui.toggleDisplayedButton(".btn-start", ".btn-new");
+        }
+        else {
+          ui.toggleDisplayedButton(".btn-restart", ".btn-new");
+        }
       }
       else {
         ui.inputErrorMessage();
@@ -28,17 +38,8 @@
       return false;
     },
 
-    play: function() {
-      var _this = this;
-      _this.newGame();
-      if (this.choicePlayer()) {
-        return;
-      }
-      $("tr td").click(function() {
-        _this.human.choiceSpot(_this.board, _this.currentPlayer);
-        _this.nextTurn();
-        _this.currentPlayer = _this.human.changePlayer(_this.currentPlayer);
-      });
+    winner: function() {
+      return this.currentPlayer == "X" ? "Player 1" : "Player 2";
     },
 
     nextTurn: function() {
@@ -56,8 +57,20 @@
       }
     },
 
-    winner: function() {
-      return this.currentPlayer == "X" ? "Player 1" : "Player 2";
+    play: function() {
+      var _this = this;
+      if (_this.choicePlayer()) {
+        return;
+      }
+      if (_this.newGame()) {
+        $("tr td").unbind();
+        return;
+      }
+      $("tr td").click(function() {
+        _this.human.choiceSpot(_this.board, _this.currentPlayer);
+        _this.nextTurn();
+        _this.currentPlayer = _this.human.changePlayer(_this.currentPlayer);
+      });
     },
 
     startGame: function() {
@@ -65,29 +78,30 @@
       ui.hideButton(".btn-restart", ".btn-new");
       $(".btn-start").click(function(e) {
         _this.play();
-      });
-    },
-
-    newGame: function() {
-      var _this = this;
-      $(".btn-new").click(function() {
-        _this.resetGame();
-        _this.choicePlayer();
-      });
-    },
-
-    restartGame: function() {
-      var _this = this;
-      $(".btn-restart").one("click", function() {
-        ui.toggleDisplayedButton(".btn-restart", ".btn-new");
-        _this.resetGame();
-        _this.play();
+        e.stopPropagation();
       });
     },
 
     resetGame: function() {
       $("tr td").empty();
       this.board.resetBoard();
+    },
+
+    newGame: function() {
+      var _this = this;
+      $(".btn-new").click(function() {
+        _this.resetGame();
+        return _this.choicePlayer();
+      });
+    },
+
+    restartGame: function() {
+      var _this = this;
+      $(".btn-restart").click(function(e) {
+        _this.resetGame();
+        _this.play();
+        e.stopPropagation();
+      });
     }
   };
   window.Game = Game;
