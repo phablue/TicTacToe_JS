232f88d14b89d93b86c59e87d54e8b979f7aa10f
hyunahpark <phablue@gmail.com>
2014-03-11 13:55:04 -0500

    Test Game.js

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,12 +2,12 @@ describe ("Test Game", function() {
   describe ("The message pops up", function() {
     var winMessage;
     var tieMessage;
-    var unbind;
+    var visualAfterGameOver;
 
     beforeEach (function() {
       winMessage = spyOn(UI, "winMessage");
       tieMessage = spyOn(UI, "tieMessage");
-      unbind = spyOn($.fn, "unbind")
+      visualAfterGameOver = spyOn(Game, "visualAfterGameOver");
     });
 
     describe ("Test Message", function() {
@@ -15,14 +15,14 @@ describe ("Test Game", function() {
         GameBoard.spots = ["X", "X", "X", "O", 5, "O", 7, 8, 9];
         Game.nextTurn();
         expect(winMessage).toHaveBeenCalled();
-        expect(unbind).toHaveBeenCalled();
+        expect(visualAfterGameOver).toHaveBeenCalled();
       });
 
       it ("Pops up for tie and click event doesn't work,if the Game tie.", function() {
         GameBoard.spots = ["X", "O", "X", "O", "X", "X", "O", "X", "O"];
         Game.nextTurn();
         expect(tieMessage).toHaveBeenCalled();
-        expect(unbind).toHaveBeenCalled();
+        expect(visualAfterGameOver).toHaveBeenCalled();
       });
 
       it ("Not Pops up any message and click event works,if the Game is not won or tied.", function() {
@@ -30,10 +30,33 @@ describe ("Test Game", function() {
         Game.nextTurn();
         expect(winMessage).not.toHaveBeenCalled();
         expect(tieMessage).not.toHaveBeenCalled();
-        expect(unbind).not.toHaveBeenCalled();
+        expect(visualAfterGameOver).not.toHaveBeenCalled();
       });
     });
   });
+
+  describe ("Test visual function", function() {
+    var toggleDisplayedButton;
+    var restartGame;
+    var unbind;
+    var click;
+
+    beforeEach (function() {
+      toggleDisplayedButton = spyOn(UI, "toggleDisplayedButton");
+      restartGame = spyOn(Game, "restartGame");
+      unbind = spyOn($.fn, "unbind");
+      click = spyOnEvent('tr td', 'click');
+    });
+
+    it ("Test call function after game over", function() {
+      Game.visualAfterGameOver();
+      expect(unbind).toHaveBeenCalled();
+      expect(click).not.toHaveBeenTriggered();
+      expect(toggleDisplayedButton).toHaveBeenCalled();
+      expect(restartGame).toHaveBeenCalled();
+    });
+  });
+
   describe ("Return the player", function() {
     var thePlayer;
 
@@ -48,5 +71,45 @@ describe ("Test Game", function() {
       thePlayer = Game.winner();
       expect(thePlayer).toBe("Player 2");
     });
+  });
+
+  describe ("Teste choicePlayer function", function() {
+    var input;
+    var visualAfterChoice;
+
+    beforeEach(function() {
+      Game.currentPlayer = "";
+      visualAfterChoice = spyOn(Game, "visualAfterChoice");
+    })
+
+    it ("If input is null return true", function() {
+      var askChoicePlayer = spyOn(UI, "askChoicePlayer").and.returnValue(null);
+      input = askChoicePlayer;
+      expect(Game.choicePlayer()).toBeTruthy();
+    })
+
+    it ("If input is '' return true", function() {
+      var askChoicePlayer = spyOn(UI, "askChoicePlayer").and.returnValue("");
+      input = askChoicePlayer;
+      expect(Game.choicePlayer()).toBeTruthy();
+    })
+
+    it ("If input is 'X', will call visualAfterChoice() and will change current player", function() {
+      var askChoicePlayer = spyOn(UI, "askChoicePlayer").and.returnValue("X");
+      input = askChoicePlayer;
+      Game.choicePlayer();
+      expect(Game.currentPlayer).toBe("X");
+      expect(visualAfterChoice).toHaveBeenCalled();
+      expect(Game.choicePlayer()).toBeFalsy();
+    })
+
+    it ("If input is 'O', will call visualAfterChoice() and will change current player", function() {
+      var askChoicePlayer = spyOn(UI, "askChoicePlayer").and.returnValue("O");
+      input = askChoicePlayer;
+      Game.choicePlayer();
+      expect(Game.currentPlayer).toBe("O");
+      expect(visualAfterChoice).toHaveBeenCalled();
+      expect(Game.choicePlayer()).toBeFalsy();
+    })
   });
 });
