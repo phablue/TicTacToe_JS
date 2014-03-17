28ea9288fa412971ccdec4412c21692731a5e8bf
hyunahpark <phablue@gmail.com>
2014-03-17 14:54:57 -0500

    figure out human test

--- spec/HumanSpec.js
+++ spec/HumanSpec.js
@@ -1,37 +1,40 @@
 describe ("Test Human", function() {
   var currentPlayer;
-
-  beforeEach(function () {
-    GameBoard.spots = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  });
+  var e;
 
   describe("Sets value of selected elements", function() {
+    beforeEach(function() {
+      GameBoard.spots = [1, "X", 3];
+      setFixtures(' <table> <tr> \
+                      <td id = "0"></td> \
+                      <td id = "1">X</td> \
+                      <td id = "2"></td> \
+                    </tr> </table>');
+    });
+
     it ("td #0 and board[0] value changes to 'X' and current player changes to 'O'", function() {
       currentPlayer = "X";
-      setFixtures("<tr> <td id = '0'></td></tr>");
       expect ($("tr td")).toBeEmpty();
-      Human.choiceSpot(GameBoard, currentPlayer);
-      $("tr td").click();
+      e = $("#0").click();
+      Human.choiceSpot(e, GameBoard, _this.currentPlayer);
       expect ($("#0")).toHaveText("X");
       expect (GameBoard.spots[0]).toBe("X");
     });
 
-    it ("td #3 and board[3] value changes to 'O' and current player changes to 'X'", function() {
+    it ("td #2 and board[2] value changes to 'O' and current player changes to 'X'", function() {
       currentPlayer = "O";
-      setFixtures("<tr> <td id = '3'></td></tr>");
-      expect ($("tr td")).toBeEmpty();
-      Human.choiceSpot(GameBoard, currentPlayer);
-      $("tr td").click();
-      expect ($("#3")).toHaveText("O");
-      expect (GameBoard.spots[3]).toBe("O");
+      expect ($("#2")).toBeEmpty();
+      e = $('#2').trigger("click");
+      Human.choiceSpot(e, GameBoard, _this.currentPlayer);
+      expect ($("#2")).toHaveText("O");
+      expect (GameBoard.spots[2]).toBe("O");
     });
 
     it ("Can't set of a choosen spot", function() {
       var errorMessage = spyOn(UI, "spotErrorMessage");
       currentPlayer = "O";
-      setFixtures("<tr> <td id = '0'>X</td><td id = '3'></td></tr>");
-      Human.choiceSpot(GameBoard, currentPlayer);
-      $("#0").click();
+      e = $('#1').trigger("click");
+      Human.choiceSpot(e, GameBoard, _this.currentPlayer);
       expect (errorMessage).toHaveBeenCalled();
     });
   });
