458e0c9316f82f6658e8322db896016929e52bb5
hyunahpark <phablue@gmail.com>
2014-03-18 16:19:03 -0500

    delete introGame function test in Game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -90,34 +90,6 @@ describe ("Test Game", function() {
     });
   });
 
-  describe ("Test introGame function", function() {
-    var firstMove;
-    var newGame;
-    var play;
-
-    it ("Call other function", function() {
-      firstMove = spyOn(Game, "firstMove");
-      newGame = spyOn(UI, "newGame");
-      Game.introGame();
-      expect(firstMove).toHaveBeenCalled();
-      expect(newGame).toHaveBeenCalled();
-    });
-
-    it ("if goFirst is true return", function() {
-      play = spyOn(Game, "play");
-      firstMove = spyOn(Game, "firstMove").and.returnValue(true);
-      Game.introGame();
-      expect(play).not.toHaveBeenCalled();
-    });
-
-    it ("if goFirst is not true call play", function() {
-      firstMove = spyOn(Game, "firstMove").and.returnValue("");
-      play = spyOn(Game, "play");
-      Game.introGame();
-      expect(play).toHaveBeenCalled();
-    });
-  });
-
   describe ("Test play function", function() {
     var humanPlay;
     var computerPlay;
